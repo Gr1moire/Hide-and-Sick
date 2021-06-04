fe564ea9b0a0b807e4c527d3f197d156a8bb4235
Pascal Betti <pascal.betti@epitech.eu>
2021-06-04 16:47:22 +0100

    Added player scene

---
+++ player/Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+export var ACCELERATION = 750
+export var MAX_SPEED = 100
+export var ROLL_SPEED = 130
+export var FRICTION = 700
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK    
+}
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+var velocity = Vector2.ZERO
+var state = MOVE
+
+func _ready():
+	randomize()
+	animationTree.active = true
+
+func _physics_process(delta):
+			
+	match state:
+		MOVE:
+			move_state(delta)
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationState.travel("Walk")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION *delta)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)    
+		
+	move()
+
+func move():
+	velocity = move_and_slide(velocity)
